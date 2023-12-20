74bbc7f5e72841244841079f17700fb8961af2f1
i-whammy <yas.iwami@gmail.com>
2023-12-20 21:08:39 +0900

    supported java 21

--- src/main/kotlin/dev/iwhammy/GaugeAnnotationProcessor.kt
+++ src/main/kotlin/dev/iwhammy/GaugeAnnotationProcessor.kt
@@ -4,29 +4,20 @@ import com.thoughtworks.gauge.Step
 import javax.annotation.processing.AbstractProcessor
 import javax.annotation.processing.RoundEnvironment
 import javax.annotation.processing.SupportedAnnotationTypes
-import javax.lang.model.element.Element
+import javax.annotation.processing.SupportedSourceVersion
+import javax.lang.model.SourceVersion
 import javax.lang.model.element.TypeElement
-import javax.lang.model.type.ExecutableType
-import javax.lang.model.type.TypeMirror
 
 @SupportedAnnotationTypes("com.thoughtworks.gauge.Step")
-//@SupportedSourceVersion(SourceVersion.RELEASE_21)
+@SupportedSourceVersion(SourceVersion.RELEASE_21)
 class GaugeAnnotationProcessor: AbstractProcessor() {
     override fun process(annotations: MutableSet<out TypeElement>, roundEnv: RoundEnvironment): Boolean {
         val collections = annotations
             .flatMap(roundEnv::getElementsAnnotatedWith)
-            .map { element -> element.toGaugeSteps() }
-        collections.forEach { gaugeStep ->
-            println(gaugeStep)
+            .map { element -> element.getAnnotation(Step::class.java).value }
+        collections.forEach { gaugeSteps ->
+            gaugeSteps.forEach { println(it) }
         }
         return true
     }
-}
-
-data class GaugeStep(val stepText: String, val types: List<TypeMirror>)
-
-fun Element.toGaugeSteps(): GaugeStep {
-    val stepText = this.getAnnotation(Step::class.java).value
-    val types = (this.asType() as ExecutableType).parameterTypes
-    return GaugeStep(stepText.first(), types)
 }
