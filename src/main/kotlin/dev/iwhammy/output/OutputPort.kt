55403e432ecbbac9a163fbe0586d5b7386b4efb2
i-whammy <yas.iwami@gmail.com>
2023-12-26 22:03:00 +0900

    add javadoc

--- src/main/kotlin/dev/iwhammy/output/OutputPort.kt
+++ src/main/kotlin/dev/iwhammy/output/OutputPort.kt
@@ -1,5 +1,14 @@
 package dev.iwhammy.output
 
+/**
+ * Interface for output of annotation values.
+ *
+ * @since 0.1.0
+ */
 interface OutputPort {
+    /**
+     * @param annotations List of annotation values
+     * @return Unit
+     */
     fun output(annotations: List<Array<String>>): Unit
 }
